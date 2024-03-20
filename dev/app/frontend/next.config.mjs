b727f36cfa98b622e34c82bae9274cd58393690c
masafumi330 <mihusamawasami@gmail.com>
2024-03-20 12:59:38 +0900

    :zap: stateを使用。typescriptでサーバーAPIを作成

--- dev/app/frontend/next.config.mjs
+++ dev/app/frontend/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    async rewrites() {
+        return [
+        {
+            source: '/api/:path*',
+            destination: 'http://host.docker.internal:4000/api/:path*',
+        },
+        ]
+    },
+};
 
 export default nextConfig;
--- dev/app/frontend/src/app/hello/page.tsx
+++ dev/app/frontend/src/app/hello/page.tsx
@@ -1,3 +1,16 @@
+'use client';
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
-  }
+    const [data, setData] = useState({name: ""});
+
+    useEffect(() => {
+        axios.get("/api/hello")
+          .then((res) => res.data)
+          .then((data) => setData(data));
+    }, []);
+
+    return <div>Hello, { data.name }</div>;
+}
