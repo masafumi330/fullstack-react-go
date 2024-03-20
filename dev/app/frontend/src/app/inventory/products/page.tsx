347675c360366fa346588778be246154fb7c6867
masafumi330 <mihusamawasami@gmail.com>
2024-03-20 15:26:29 +0900

    :zap: mockデータで商品一覧を描画

--- dev/app/frontend/src/app/inventory/products/page.tsx
+++ dev/app/frontend/src/app/inventory/products/page.tsx
@@ -1,11 +1,40 @@
-// 商品一覧画面
+'use client';
+
+import { useEffect, useState } from "react";
+import productsData from "./sample.json";
+
+type Product = {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+}
 
 export default function Page() {
+    const [products, setProducts] = useState<Product[]>(productsData);
+    useEffect(() => {
+        setProducts(productsData);
+    }, []);
+
     return (
         <>
             <h2>商品一覧</h2>
-            <p>商品の一覧を表示</p>
+            <table>
+                <tbody>
+                    {products.map((product: any) => (
+                        <tr key={product.id}>
+                            <td>{product.id}</td>
+                            <td>{product.name}</td>
+                            <td>{product.price}</td>
+                            <td>{product.description}</td>
+                            <td>
+                                <button>更新/削除</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </>
-        
+
     )
 }
